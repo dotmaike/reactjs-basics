57e1653b350199e706bf39f41c89a00c8e840da6
Miguel Murillo <miguel_murillo@epam.com>
2017-05-04 08:22:05 -0500

    h1 replaced

--- __test__/scenes/Home/Home.test.js
+++ __test__/scenes/Home/Home.test.js
@@ -3,6 +3,6 @@ import { render } from 'react-dom';
 import Home from './../../../src/scenes/Home/Home.jsx';
 
 it('render Home', () => {
-  const h1 = document.createElement('h1');
-  render(<Home />, h1);
+  const h3 = document.createElement('h3');
+  render(<Home />, h3);
 });
